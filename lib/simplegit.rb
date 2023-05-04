e13b1b04057171d4cf71f957f72b61b22d032495
Jesus Manuel <jesumapa10@gmail.com>
2023-05-04 01:19:28 -0500

    small tweak

--- lib/simplegit.rb
+++ lib/simplegit.rb
@@ -10,19 +10,10 @@ class SimpleGit
   end
 
 #  private
-  
-<<<<<<< HEAD
+
 #    def command(git_cmd)
 #      Dir.chdir(@git_dir) do
 #        return `#{git_cmd} 2>&1`.chomp
 #      end
-#    end
-=======
-    def command(git_cmd)
-      Dir.chdir(@git_dir) do
-        return `#{git_cmd} 5>&1`.chomp
-      end
-    end
->>>>>>> 487089f502d07abcaded4be5acf271d8bf1d3840
-  
+#    end  
 end
